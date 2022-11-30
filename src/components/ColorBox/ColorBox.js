8badbed26144e08b60b4763005f2f56471fb6f00
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-11-30 18:16:01 +0100

    styling color box component

--- src/components/ColorBox/ColorBox.js
+++ src/components/ColorBox/ColorBox.js
@@ -6,9 +6,14 @@ class ColorBox extends Component {
 	render() {
 		const { color, name } = this.props;
 		return (
-			<div style={{ backgroundColor: color }} className="ColorBox">
-				<span>{name}</span>
-				<span>MORE</span>
+			<div style={{ background: color }} className="ColorBox">
+				<div className="ColorBox__copy-container">
+					<div className="ColorBox__box-content">
+						<span>{name}</span>
+					</div>
+					<button className="ColorBox__copy-btn">Copy</button>
+				</div>
+				<span className="ColorBox__see-more">MORE</span>
 			</div>
 		);
 	}
