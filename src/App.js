607d36ca01cbf1d2889c75807eb00cbc200a05e2
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-12-03 23:08:56 +0100

    adding color helper

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 import Palette from './components/Palette/Palette.js';
 
+import { generatePalette } from './helpers/colorHelper';
+
 import seedsPalette from './assets/seedsPalette';
 
 class App extends Component {
 	render() {
+		console.log(generatePalette(seedsPalette[8]));
 		return (
 			<div>
 				<Palette {...seedsPalette[8]} />
