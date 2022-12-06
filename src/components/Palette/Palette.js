5ed3d8b5ab5a4a6f0ccf1d87fdc60b73f430f9a9
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-12-06 14:16:27 +0100

    adding state for color format changing

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -7,25 +7,34 @@ import './Palette.css';
 class Palette extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { colorLevel: 500 };
+		this.state = { colorLevel: 500, colorFormat: 'hex' };
 		this.changeColorLevel = this.changeColorLevel.bind(this);
+		this.changeColorFormat = this.changeColorFormat.bind(this);
 	}
 
 	changeColorLevel(newLevel) {
 		this.setState({ colorLevel: newLevel });
 	}
+	changeColorFormat(newFormat) {
+		this.setState((st) => ({ ...st, colorFormat: newFormat }));
+	}
 
 	render() {
-		const colorLevel = this.state.colorLevel;
+		const { colorLevel, colorFormat } = this.state;
 		const { colors } = this.props.palette;
 		const colorBoxes = colors[colorLevel].map((color, idx) => (
-			<ColorBox key={color.name + idx} {...color} />
+			<ColorBox
+				key={color.name + idx}
+				background={color[colorFormat]}
+				name={color.name}
+			/>
 		));
 		return (
 			<div className="Palette">
 				<Navbar
 					colorLevel={colorLevel}
 					onChangeColorLevel={this.changeColorLevel}
+					onChangeColorFormat={this.changeColorFormat}
 				/>
 				<div className="Palette__colors">{colorBoxes}</div>
 				{/* footer */}
