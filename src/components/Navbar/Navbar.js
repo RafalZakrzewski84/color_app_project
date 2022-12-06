9f1c8d93b7a337ee4b266c4823c9690fc5b3a1cd
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-12-06 14:17:18 +0100

    adding select input for changing color format

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Slider from 'rc-slider';
+import { Select, MenuItem } from '@mui/material';
 
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
 class Navbar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { colorFormat: 'hex' };
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(evt) {
+		this.setState({ colorFormat: evt.target.value });
+		this.props.onChangeColorFormat(evt.target.value);
+	}
+
 	render() {
+		const { colorFormat } = this.state;
 		const { colorLevel, onChangeColorLevel } = this.props;
 		return (
 			<div className="Navbar">
@@ -23,6 +36,13 @@ class Navbar extends Component {
 							onAfterChange={onChangeColorLevel}
 						/>
 					</div>
+				</div>
+				<div className="Navbar__select-container">
+					<Select value={colorFormat} onChange={this.handleChange}>
+						<MenuItem value="hex">HEX - #ffffff</MenuItem>
+						<MenuItem value="rgb">RGB - (255,255,255)</MenuItem>
+						<MenuItem value="rgba">RGBA - (255,255,255,1)</MenuItem>
+					</Select>
 				</div>
 			</div>
 		);
