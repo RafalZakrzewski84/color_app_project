0291ed8e0594f7a4e3e518bef137088e8c8c28fd
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-12-06 22:43:33 +0100

    adding react-router and basic routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import Palette from './components/Palette/Palette.js';
 
@@ -9,9 +10,13 @@ import seedsPalette from './assets/seedsPalette';
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<Palette palette={generatePalette(seedsPalette[0])} />
-			</div>
+			<Routes>
+				<Route path="/" element={<h1>Palettes go here!</h1>} />
+				<Route path="/palette/:id" element={<h1>Palette</h1>} />
+			</Routes>
+			// <div>
+			// 	<Palette palette={generatePalette(seedsPalette[0])} />
+			// </div>
 		);
 	}
 }
