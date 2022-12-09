511b360f5cf6a3a0f4dcef34fc900577b717f9ff
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-12-09 22:43:34 +0100

    Moving ThemeProvider to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import Palette from './components/Palette/Palette.js';
 import PaletteList from './components/PaletteList/PaletteList.js';
@@ -7,30 +8,33 @@ import PaletteList from './components/PaletteList/PaletteList.js';
 import { generatePalette } from './helpers/colorHelper';
 import seedsPalette from './assets/seedsPalette';
 
+const theme = createTheme();
 class App extends Component {
 	findPalette(id) {
 		return seedsPalette.find((palette) => palette.id === id);
 	}
 	render() {
 		return (
-			<Switch>
-				<Route
-					exact
-					path="/"
-					render={() => <PaletteList palettes={seedsPalette} />}
-				/>
-				<Route
-					exact
-					path="/palette/:id"
-					render={(renderParams) => (
-						<Palette
-							palette={generatePalette(
-								this.findPalette(renderParams.match.params.id)
-							)}
-						/>
-					)}
-				/>
-			</Switch>
+			<ThemeProvider theme={theme}>
+				<Switch>
+					<Route
+						exact
+						path="/"
+						render={() => <PaletteList palettes={seedsPalette} />}
+					/>
+					<Route
+						exact
+						path="/palette/:id"
+						render={(renderParams) => (
+							<Palette
+								palette={generatePalette(
+									this.findPalette(renderParams.match.params.id)
+								)}
+							/>
+						)}
+					/>
+				</Switch>
+			</ThemeProvider>
 		);
 	}
 }
--- src/components/MiniPalette/MiniPalette.js
+++ src/components/MiniPalette/MiniPalette.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
 import { unstable_styleFunctionSx } from '@mui/system';
-import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme();
 const Div = styled('div')(unstable_styleFunctionSx);
 const H5 = styled('h5')(unstable_styleFunctionSx);
 
@@ -11,34 +9,32 @@ const MiniPalette = (props) => {
 	console.log(props);
 	const { colors, paletteName, emoji } = props;
 	return (
-		<ThemeProvider theme={theme}>
-			<Div
+		<Div
+			sx={{
+				backgroundColor: 'white',
+				border: '1px solid black',
+				borderRadius: '5px',
+				padding: '0.5rem',
+				position: 'relative',
+				overflow: 'hover',
+				'&:hover': { cursor: 'pointer' },
+			}}>
+			<Div sx={{ backgroundColor: 'grey' }}>Colors</Div>
+			<H5
 				sx={{
-					backgroundColor: 'white',
-					border: '1px solid black',
-					borderRadius: '5px',
-					padding: '0.5rem',
+					display: 'flex',
+					justifyContent: 'space-between',
+					alignItems: 'center',
+					margin: '0',
+					color: 'black',
+					paddingTop: '0.5rem',
+					fontSize: '1rem',
 					position: 'relative',
-					overflow: 'hover',
-					'&:hover': { cursor: 'pointer' },
 				}}>
-				<Div sx={{ backgroundColor: 'grey' }}>Colors</Div>
-				<H5
-					sx={{
-						display: 'flex',
-						justifyContent: 'space-between',
-						alignItems: 'center',
-						margin: '0',
-						color: 'black',
-						paddingTop: '0.5rem',
-						fontSize: '1rem',
-						position: 'relative',
-					}}>
-					{paletteName}
-					<span sx={{ marginLeft: '0.5rem', fontSize: '1.5rem' }}>{emoji}</span>
-				</H5>
-			</Div>
-		</ThemeProvider>
+				{paletteName}
+				<span sx={{ marginLeft: '0.5rem', fontSize: '1.5rem' }}>{emoji}</span>
+			</H5>
+		</Div>
 	);
 };
 
