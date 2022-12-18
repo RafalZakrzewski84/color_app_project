12b3c74718c227e120892156d7215fe98f84f512
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-12-18 21:23:13 +0100

    styling palette list

--- src/components/PaletteList/PaletteList.js
+++ src/components/PaletteList/PaletteList.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { unstable_styleFunctionSx } from '@mui/system';
 
 import MiniPalette from '../MiniPalette/MiniPalette';
+
+const Div = styled('div')(unstable_styleFunctionSx);
+const Nav = styled('nav')(unstable_styleFunctionSx);
 
 export class PaletteList extends Component {
 	render() {
@@ -10,10 +14,41 @@ export class PaletteList extends Component {
 			<MiniPalette key={palette.id} {...palette} />
 		));
 		return (
-			<div className="PaletteList">
-				<h1>React Colors</h1>
-				{miniPalettes}
-			</div>
+			<Div
+				sx={{
+					background: 'blue',
+					height: '100%',
+					display: 'flex',
+					alignItems: 'flex-start',
+					justifyContent: 'center',
+				}}>
+				<Div
+					sx={{
+						width: '50%',
+						display: 'flex',
+						alignItems: 'flex-start',
+						flexDirection: 'column',
+					}}>
+					<Nav
+						sx={{
+							display: 'flex',
+							width: '100%',
+							justifyContent: 'space-between',
+							color: 'white',
+						}}>
+						<h1>React Colors</h1>
+					</Nav>
+					<Div
+						sx={{
+							width: '100%',
+							display: 'grid',
+							gridTemplateColumns: 'repeat(3, 30%)',
+							gridGap: '5%',
+						}}>
+						{miniPalettes}
+					</Div>
+				</Div>
+			</Div>
 		);
 	}
 }
